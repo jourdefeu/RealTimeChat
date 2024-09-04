cd73d154d8cf28f923a5772c2417685dcb864d3b
Valerie <157377780+jourdefeu@users.noreply.github.com>
2024-09-04 22:17:06 +0300

    Add files via upload

---
+++ client/scr/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './app.css'
+import LongPulling from "./LongPulling";
+import EventSourcing from "./EventSourcing";
+import WebSock from "./LongPulling";
+
+
+function App() {
+
+  return (
+      <div>
+        <LongPulling/>
+      </div>
+  )
+}
+
+
+export default App;
---
+++ client/scr/LongPulling.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LongPulling = () => {
+    return (
+        <div className="center">
+            <div>
+                <div className="form">
+                    <input type="text"/>
+                    <button>Отправить</button>
+                </div>
+            </div>
+        </div>
+    )
+}
